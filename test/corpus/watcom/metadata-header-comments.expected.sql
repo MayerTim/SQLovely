0ed967fe6f9c7e067a56b9072e3bd90fa66e3619
MayerTim <tim97mayer@googlemail.com>
2026-06-10 20:03:15 +0200

    test(formatter): cover corpus edge cases for protected text

---
+++ test/corpus/watcom/metadata-header-comments.expected.sql
@@ -0,0 +1,10 @@
+-- METADATA
+-- Name        : dbo.metadata_fixture
+-- Version     : 1.0.0
+-- Description : Public-safe metadata header fixture.
+-- History     :
+--   2026-06-10 - tester - Created fixture.
+CREATE PROCEDURE dbo.metadata_fixture()
+BEGIN
+  SELECT 1;
+END;
